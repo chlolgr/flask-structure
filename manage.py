b47c627ed6f6c753c453b83eae35d89b876d1100
Chloé Lagrue <36328702+chlolgr@users.noreply.github.com>
2022-07-01 20:34:19 +0200

    added post-comma spaces

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from flask_script import Manager
-from flask_migrate import Migrate,MigrateCommand
-from app import app,db
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
 
 
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 manager = Manager(app)
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__=='__main__': 
